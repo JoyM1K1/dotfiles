2a4dd1a05180533d472509bfc38b467e2c82d9f0
JoyM1K1 <kohchanman14@gmail.com>
2021-01-16 07:03:11 +0900

    add vscode

---
+++ .vscode/install.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+DOTVSCODEDIR=${0:a:h}
+VSCODE_SETTING_DIR=$HOME/Library/Application\ Support/Code\ -\ Insiders/User
+
+files=("settings.json" "keybindings.json")
+for file in $files; do
+    if [[ -f $VSCODE_SETTING_DIR/$file ]]; then
+        echo "replace settings.json with symbolic link"
+        rm -f "$VSCODE_SETTING_DIR/$file"
+    fi
+    ln -s "$DOTVSCODEDIR/$file" "$VSCODE_SETTING_DIR/$file"
+done
