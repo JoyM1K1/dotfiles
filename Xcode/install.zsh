5d8699328aadb85e59c641569e44d5bd2c94a440
JoyM1K1 <kohchanman14@gmail.com>
2022-01-03 04:17:59 +0900

    version分けに対応

--- Xcode/install.zsh
+++ Xcode/install.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
 CURRENT_DIR=${0:a:h}
+XCODE_VERSION=${1:+"-$1"}
 
-XCODE_RESOURCES_DIR=/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources
+XCODE_RESOURCES_DIR=/Applications/Xcode${XCODE_VERSION}.app/Contents/Frameworks/IDEKit.framework/Versions/A/Resources
 XCODE_USER_DATA_DIR=$HOME/Library/Developer/Xcode/UserData
 
 if [[ -d $XCODE_RESOURCES_DIR ]]; then
