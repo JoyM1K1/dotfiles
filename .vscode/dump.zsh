4615430216426cb04fcc25cefdacdcbcdf05a512
JoyM1K1 <kohchanman14@gmail.com>
2022-01-15 00:10:45 +0900

    rename .vscode with vscode

--- .vscode/dump.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-DOTVSCODEDIR=${0:a:h}
-code --list-extensions > "${DOTVSCODEDIR}/extensions"
--- vscode/install.zsh
+++ vscode/install.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-DOTVSCODEDIR=${0:a:h}
+DOT_VSCODE_DIR=${0:a:h}
+EXTENSIONS_FILENAME=${1:-extensions}
+
 VSCODE_SETTING_DIR=$HOME/Library/Application\ Support/Code/User
 
 files=("settings.json" "keybindings.json")
@@ -9,12 +11,11 @@ for file in $files; do
         echo "replace settings.json with symbolic link"
         rm -f "$VSCODE_SETTING_DIR/$file"
     fi
-    ln -s "$DOTVSCODEDIR/$file" "$VSCODE_SETTING_DIR/$file"
+    ln -s "$DOT_VSCODE_DIR/$file" "$VSCODE_SETTING_DIR/$file"
 done
 
 # extensions
-cat extensions | while read line
+cat "${DOT_VSCODE_DIR}/${EXTENSIONS_FILENAME}" | while read line
 do
     code --install-extension $line
 done
-code --list-extensions > extensions
