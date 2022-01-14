1a506348fcbe642389a417489d9ef20fd2225a73
JoyM1K1 <kohchanman14@gmail.com>
2022-01-14 13:22:44 +0900

    dump/installする Brewfileのsuffixを選択可能にした

--- Brewfile/dump.zsh
+++ Brewfile/dump.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
 CURRENT_DIR=${0:a:h}
+DUMP_FILENAME="Brewfile${BREWFILE_SUFFIX:+".${BREWFILE_SUFFIX}"}"
 
-brew bundle dump --force --file "${CURRENT_DIR}/Brewfile"
+brew bundle dump --force --file "${CURRENT_DIR}/${DUMP_FILENAME}"
--- Brewfile/install.zsh
+++ Brewfile/install.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
 CURRENT_DIR=${0:a:h}
+INSTALL_FILENAME="Brewfile${BREWFILE_SUFFIX:+".${BREWFILE_SUFFIX}"}"
 
-brew bundle --file "${CURRENT_DIR}/Brewfile"
+brew bundle --file "${CURRENT_DIR}/${INSTALL_FILENAME}"
