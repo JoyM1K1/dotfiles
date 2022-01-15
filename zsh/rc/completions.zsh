b8d06db0b0ed164eb876ec25d00a69fbe24423fc
JoyM1K1 <kohchanman14@gmail.com>
2022-01-16 02:06:49 +0900

    completionsの自動取得に対応

---
+++ zsh/rc/completions.zsh
@@ -0,0 +1,14 @@
+# _gh
+if (( $+commands[gh] )) && [[ ! -f $ZDOTDIR/functions/_gh ]]; then
+    gh completion -s zsh > $ZDOTDIR/functions/_gh
+fi
+
+# _docker https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
+if (( $+commands[docker] )) && [[ ! -f $ZDOTDIR/functions/_docker ]]; then
+    curl -fLo $ZDOTDIR/functions/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
+fi
+
+# _docker-compose https://github.com/docker/compose/blob/1.29.2/contrib/completion/zsh/_docker-compose
+if (( $+commands[docker-compose] )) && [[ ! -f $ZDOTDIR/functions/_docker-compose ]]; then
+    curl -fLo $ZDOTDIR/functions/_docker-compose https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose
+fi
