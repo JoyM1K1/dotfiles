16b8aae25be0de6855c4df16df67dac6f7413525
JoyM1K1 <kohchanman14@gmail.com>
2022-03-03 13:09:06 +0900

    fix upgrade

--- git_upgrade.sh
+++ git_upgrade.sh
@@ -2,13 +2,13 @@
 
 # git install
 
-sudo yum remove -y git
-sudo yum install -y curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel autoconf asciidoc xmlto docbook2X make gcc wget
-sudo ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
+yum remove -y git
+yum install -y curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel autoconf asciidoc xmlto docbook2X make gcc wget
+ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
 if [ ! -d "/usr/local/src" ]; then
-	sudo mkdir -p /usr/local/src
+	mkdir -p /usr/local/src
 fi
-sudo sh -c 'cd /usr/local/src &&
+sh -c 'cd /usr/local/src &&
 wget https://www.kernel.org/pub/software/scm/git/git-2.30.0.tar.gz &&
 tar -zxvf git-2.30.0.tar.gz &&
 cd git-2.30.0 &&
--- vim/vim_upgrade.sh
+++ vim/vim_upgrade.sh
@@ -2,12 +2,12 @@
 
 # vim install
 
-sudo yum install -y make gcc ncurses-devel
+yum install -y make gcc ncurses-devel
 if [ ! -d "/usr/local/src" ]; then
-	sudo mkdir -p /usr/local/src
+	mkdir -p /usr/local/src
 fi
-sudo git clone https://github.com/vim/vim.git /usr/local/src/vim
-sudo sh -c 'cd /usr/local/src/vim && ./configure \
+git clone https://github.com/vim/vim.git /usr/local/src/vim
+sh -c 'cd /usr/local/src/vim && ./configure \
   --disable-selinux \
   --enable-cscope \
   --enable-fontset \
