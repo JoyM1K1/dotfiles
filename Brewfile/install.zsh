d949dfe23a2bc9b77a05a9a207600c550ab9821e
JoyM1K1 <kohchanman14@gmail.com>
2024-02-13 03:56:26 +0900

    🐛 fix completions

--- Brewfile/install.zsh
+++ Brewfile/install.zsh
@@ -4,3 +4,6 @@ CURRENT_DIR=${0:a:h}
 INSTALL_FILENAME="Brewfile${BREWFILE_SUFFIX:+".${BREWFILE_SUFFIX}"}"
 
 brew bundle --file "${CURRENT_DIR}/${INSTALL_FILENAME}"
+
+# completionを読み込ませるために必要
+chmod -R go-w /opt/homebrew/share
--- zsh/rc/completions.zsh
+++ zsh/rc/completions.zsh
@@ -1,19 +1,3 @@
-# _gh
-if (( $+commands[gh] )) && [[ ! -f $ZDOTDIR/functions/_gh ]]; then
-    gh completion -s zsh > $ZDOTDIR/functions/_gh
-fi
-
-# _docker https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
-if (( $+commands[docker] )) && [[ ! -f $ZDOTDIR/functions/_docker ]]; then
-    curl -fLo $ZDOTDIR/functions/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
-fi
-
-# _docker-compose https://github.com/docker/compose/blob/1.29.2/contrib/completion/zsh/_docker-compose
-if (( $+commands[docker-compose] )) && [[ ! -f $ZDOTDIR/functions/_docker-compose ]]; then
-    curl -fLo $ZDOTDIR/functions/_docker-compose https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose
-fi
-
-# aws https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/cli-configure-completion.html#cli-command-completion-linux
-if (( $+commands[aws_completer] )); then
-	complete -C "${commands[aws_completer]}" aws
+if (( $+commands[rustup] )) && [[ ! -f $ZDOTDIR/functions/_rustup ]]; then
+	rustup completions zsh > $ZDOTDIR/functions/_rustup
 fi
