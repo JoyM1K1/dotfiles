e94f7dd18eb50e2dce4b6021a7774e74a72e242a
JoyM1K1 <kohchanman14@gmail.com>
2026-03-10 04:53:29 +0900

    vim: update dein installer

--- vim/install.zsh
+++ vim/install.zsh
@@ -4,7 +4,7 @@ source "${CURRENT_DIR}/../lib/utils.zsh"
 
 if [[ ! -d ${CURRENT_DIR}/bundles/repos/github.com/Shougo/dein.vim ]]; then
     if [[ ! -f "${CURRENT_DIR}/dein_installer.sh" ]]; then
-        curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "${CURRENT_DIR}/dein_installer.sh"
+        curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh > "${CURRENT_DIR}/dein_installer.sh"
     fi
     zsh "${CURRENT_DIR}/dein_installer.sh" "${CURRENT_DIR}/bundles"
 fi
