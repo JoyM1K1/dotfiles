159a1793f82ec3f26809b8b8adea4b472cb763d9
JoyM1K1 <kohchanman14@gmail.com>
2026-03-17 10:37:58 +0900

    nvim: 背景透過

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local function clear_bg()
+  local groups = {
+    "Normal",
+    "NormalNC",
+    "NormalFloat",
+    "SignColumn",
+    "FloatBorder",
+  }
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+  end
+end
+
+local grp = vim.api.nvim_create_augroup("transparent_background", { clear = true })
+vim.api.nvim_create_autocmd("ColorScheme", { group = grp, callback = clear_bg })
+vim.api.nvim_create_autocmd("VimEnter", { group = grp, callback = clear_bg })
